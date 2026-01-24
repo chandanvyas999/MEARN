1308424929fd99ce681ba37c1942beafadb72cb3
chandanvyas999 <cvhindu999@gmail.com>
2026-01-24 21:18:05 +0530

    Bhai aaj Part 7 me aaya aur kal khatam pakka

--- Java Script/Part6.js
+++ Java Script/Part6.js
@@ -33,3 +33,62 @@ function printTable(number) {
     }
 }
 printTable(5);
+
+//Scope 
+let sum = 10; //Global Scope
+function aa(a , b) {
+    let sum = a+b;  //Local Scope
+    console.log(sum);
+}
+aa(2,3);
+
+//Lexical scope (Nested function)
+function outerfunction() {
+    let x = 4;
+    let y = 5;
+    function innerfunction() {
+        console.log(x);
+    }
+}
+
+//Higher Order Function
+function multipleGreet(funct , count) {          //Higher order function
+    for (let i = 1; i <= count; i++)
+    funct();
+}
+let greet = function() {
+    console.log('Hello Bhai');
+}
+
+multipleGreet(greet , 5);
+
+//Methods
+let ba = {
+    sum: function(a,b) {
+        return a + b;
+    },
+    sub (a , b) {
+        return a - b;
+    },
+    mul (a , b) {
+        return a * b;
+    },
+    div (a , b) {
+        return a / b;
+    }
+};
+
+//"this" keyword of javascript
+
+let bo = {
+    name: "Chandan",
+    age: 21,
+    mathe: 56,
+    eng: 45,
+    hindi: 78,
+    getAvg() {
+        let avg = (this.mathe + this.eng + this.hindi) / 3;
+        console.log(avg);
+    }
+}
+bo.getAvg();
