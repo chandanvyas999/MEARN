f4b9542c1f0b098cb030de22233b28e9f5cb4076
chandanvyas999 <cvhindu999@gmail.com>
2026-02-14 23:31:35 +0530

    Busy Day

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -3,6 +3,19 @@ const app = express();
 
 const port = 1002;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/register", (req, res) => {
+    let { user, password } = req.query;
+    res.send(`Ye apna responce he aur ye ji apne user : ${user}`);
+});
+
+app.post("/register", (req, res) => {
+    let { user, password } = req.body;
+    res.send(`Ye POST request hai aur use ji bhaisahab ${user}`);
+});
+
 app.listen(port, () => {
     console.log(`Ho raha he kya print zara dekhe to, Aur ye apna PORT: ${port}`);
 });
