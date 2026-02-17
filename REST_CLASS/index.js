7605ff133ac3a9fa52859312f0d23e33cb31a2d4
chandanvyas999 <cvhindu999@gmail.com>
2026-02-17 23:44:24 +0530

    Next will be SQL OR MongoDb

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -5,16 +5,39 @@ const path = require("path");
 
 app.use(express.urlencoded({extended : true}));
 
+let posts = [
+    {
+        username: "helloji",
+        content: "Kuchh nahi he"
+    },
+    {
+        username: "Mera naam he Bulla",
+        content: "Rakhta hu khulla"
+    },
+    {
+        username: "Mera naam he Pote",
+        content: "Jo apne baap ke bhi nahi hote"
+    }
+]
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req,res) => {
-    res.send("Sahi chal raha he bhai ab aage kar");
+app.get("/posts", (req,res) => {
+    res.render("index.ejs", {posts});
+})
+
+app.get("/posts/new", (req,res) => {
+    res.render("new.ejs")
+})
+
+app.post("/posts", (req,res) => {
+    console.log(req.body)
+    res.send("post request is working")
 })
 
 app.listen(port, () => {
     console.log(`This is my PORT ${port}`)
 });
-
