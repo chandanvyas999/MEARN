0514287be62b0196de9640944c56eca9edea2c3d
chandanvyas999 <cvhindu999@gmail.com>
2026-02-23 23:49:02 +0530

    Mongoose

---
+++ MONGO/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+main()
+    .then(() => {
+        console.log("connection successfull");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    age : Number,
+});
