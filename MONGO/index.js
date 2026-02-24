c7e93da90a7372e93c999e62ada4e1290781fdc5
chandanvyas999 <cvhindu999@gmail.com>
2026-02-24 23:49:54 +0530

    Mongodb Part 3

--- MONGO/index.js
+++ MONGO/index.js
@@ -15,3 +15,54 @@ const userSchema = new mongoose.Schema({
     email : String,
     age : Number,
 });
+
+const User = mongoose.model("User", userSchema);
+
+User.findOneAndUpdate({name : "Kya he be"}, {age : 34})
+
+/*User.updateOne({name : "Chandan Vyas"}, {name : "Kya he be"}).then(res => {
+    console.log(res);
+}).catch((err) => {
+    console.log(err);
+})*/
+
+/*User.find({age : {$gt: 20}}).then((res) => {
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+});*/
+
+//Single Line Data Save
+/*const user1 = new User({
+    name : "Chandan Vyas",
+    age : 21,
+    email : "Fk@gmail.com"
+})
+const user2 = new User({
+    name : "Hulu Vyas",
+    age : 23,
+    email : "Sk@gmail.com"
+})
+
+user1.save();
+user2.save();
+
+//Multiple Line Data Save
+User.insertMany([{
+    name : "Harish Vyas",
+    age : 23,
+    email : "Hv@gmail.com"
+},
+{
+    name : "LULU Vyas",
+    email : "Toramaike@gmail.com",
+    age : 34
+},
+{
+    name : "Haggu Vyas",
+    email : "hagga@gmail.com",
+    age : 56
+}]).then((data) => {
+    console.log(data);
+});*/
