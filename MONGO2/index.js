50230a12df907a9d1cf968491bae507c1b20089d
chandanvyas999 <cvhindu999@gmail.com>
2026-02-26 23:50:05 +0530

    Project

--- MONGO2/index.js
+++ MONGO2/index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended : true}));
 
 main()
     .then(() => {
@@ -23,6 +24,30 @@ app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
     console.log(chats);
     res.render("index.ejs", {chats});
+});
+
+//New Route
+app.get("/chats/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+//Create Route
+app.post("/chats", (req, res) => {
+    let {from , to , message} = req.body;
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        message : message,
+        created_at : Date.now(),
+    });
+
+    newChat.save()
+    .then((res) => {
+        console.log("Chat save ho gayi bhayi..");
+    }).catch((err) => {
+        console.log(err);
+    });
+    res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
