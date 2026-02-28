1648766f8d1538bf8197148d50a6bc189b2741c6
chandanvyas999 <cvhindu999@gmail.com>
2026-02-28 23:58:23 +0530

    Bhai backend samajhna bohot mushkil he

--- MAJORPROJECT/app.js
+++ MAJORPROJECT/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOcerride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
@@ -16,11 +19,65 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOcerride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get("/", (req, res) => {
     res.send("Hey Babe Welcome");
 });
 
-app.get("/testListing", async (req, res) => {
+//Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+//New Route
+app.get("/listings/new", (req,res) => {
+    res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+//Create Route
+app.post("/listings", async (req,res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+
+//Edite Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing})
+    res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+});
+
+/*app.get("/testListing", async (req, res) => {
     let sampleListing = new Listing({
         title : "New Home",
         description : "This is for rent",
@@ -31,8 +88,8 @@ app.get("/testListing", async (req, res) => {
     await sampleListing.save();
     console.log("Sample saved");
     res.send("Sucessful testing");
-});
+});*/
 
 app.listen(5000, () => {
-    console.log("Server is running");
+    console.log("Server is running on PORT : 5000");
 });
--- MAJORPROJECT/models/listing.js
+++ MAJORPROJECT/models/listing.js
@@ -10,11 +10,14 @@ const listingSchema = new Schema({
     },
     description : String,
     image : {
-        type : String,
-        filename: String,
-        url : String,
-        default : defaultImageUrl,
-        set : (v) => v === "" ? "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        filename: {
+            type: String,
+            default: "listingimage"
+        },
+        url: {
+            type: String,
+            default: defaultImageUrl,
+        },
     },
     price : Number,
     location : String,
